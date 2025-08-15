650df9f40d1a0387ac87f92d54eb5ff207b3e1e5
SyonR <syon@vt.edu>
2025-08-15 18:40:52 -0400

    Made the inner lines thinner
    Created a 2D array that stored the border sizes for each direction based on the fixBorder function
    
    Created a function fixBorder that changed the object's border lengths based on its position in the grid

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
+// i: number of rows (the rows)
+// j: number of columns (the elements inside of the rows)
+// n: the width of the square
+// grid: the grid
+function fixBorder(row, i, j, n) {
+
+    let square = row[j];
+    console.log(square); 
+
+    square.top = (i === 0) ? 2 : 1;
+    square.left = (j === 0) ? 2 : 1;
+    square.right = (j === n - 1) ? 2 : 1;
+    square.bottom = (i === n - 1) ? 2 : 1;
+
+    console.log(square);
+
+}
+
 const container = document.querySelector("#container");
 const grid = [];
+let n = 16
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+for (let i = 0; i < n; i++) {
+    const row = [];
+    for (let j = 0; j < n; j++) {
+        row.push({ top: 1, left: 1, right: 1, bottom: 1 });
+        const square = document.createElement("div");
+        square.classList.add("squares");
+
+        fixBorder(row, i, j, n)
+
+        square.style.borderStyle = "solid";
+        square.style.borderTopWidth = `${row[j].top}px`;
+        square.style.borderLeftWidth = `${row[j].left}px`;
+        square.style.borderRightWidth = `${row[j].right}px`;
+        square.style.borderBottomWidth = `${row[j].bottom}px`;
+
+        container.appendChild(square);
+    } 
+    grid.push(row);
 }
+
+
+
